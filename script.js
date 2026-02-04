ee093d274a08847c6f597e1568862194712f3497
Miguel Cuevas <miguel.cuevas@ufm.edu>
2026-02-03 21:35:24 -0600

    Commit No.2
    Adding the button for hidding the information and for the table of education. And i created a function called ocultarEducacion that returns a true or false through === that checks if the table was visible or not, if it was visible it turns into none and if it was hidden it turns into a block or the table basically.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const saludo = document.getElementById("saludar");
 const hora = new Date().getHours();
 
-if (hora < 12) {
+if (hora < 11) {
     saludo.textContent = "Buenos días";
-} else if (hora < 18) {
+} else if (hora < 17) {
     saludo.textContent = "Buenas tardes";
 } else {
     saludo.textContent = "Buenas noches";
+}
+
+function ocultarEducation() {
+    const educacion = document.getElementById("educacion");
+    educacion.style.display = educacion.style.display === "none" ? "block" : "none";
 }
